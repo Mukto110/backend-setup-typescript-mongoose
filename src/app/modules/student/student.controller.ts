62ceb09b314e3f1c3dcc5555036154f6c92ab555
mukto110 <merajhossainmukto@gmail.com>
2025-01-26 18:50:11 +0600

    successfully connect to mongodb, create data, get data

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -3,9 +3,9 @@ import { StudentService } from "./student.service";
 
 const createStudent = async (req: Request, res: Response) => {
   try {
-    const student = req.body;
+    const { student: studentData } = req.body;
 
-    const result = await StudentService.createStudentIntoDB(student);
+    const result = await StudentService.createStudentIntoDB(studentData);
 
     res.status(200).json({
       success: true,
@@ -17,6 +17,36 @@ const createStudent = async (req: Request, res: Response) => {
   }
 };
 
+const getAllStudent = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentService.getAllStudentFromDB();
+    res.status(200).json({
+      success: true,
+      message: "Students are retrieved successfully",
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const studentId = req.params.studentId;
+    const result = await StudentService.getSingleStudentFromDB(studentId);
+
+    res.status(200).json({
+      success: true,
+      message: "Single student found",
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const StudentControllers = {
   createStudent,
+  getAllStudent,
+  getSingleStudent,
 };
